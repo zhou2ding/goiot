fb4356ac5776462323272715f0db80ae4147b5c6
zhou2ding <458405826@qq.com>
2022-12-08 22:11:21 +0800

    删除beego相关

--- Golang学习笔记/beego_project/go.mod
+++
@@ -1,7 +0,0 @@
-module beego_project
-
-go 1.16
-
-require github.com/astaxie/beego v1.12.3
-
-require github.com/go-sql-driver/mysql v1.6.0
--- Golang学习笔记/beego_project/utils/log_template/log_template.go
+++
@@ -1,8 +0,0 @@
-package logtemplate
-
-import "fmt"
-
-func LogProcess(start_time string, cur, cnt int, use_time, end_time string) string {
-	ret := fmt.Sprintf("starttime:%v | current/count:%v/%v | use_time:%vs | endtime:%v", start_time, cur, cnt, use_time, end_time)
-	return ret
-}
