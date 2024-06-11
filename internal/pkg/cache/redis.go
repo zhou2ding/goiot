2bcbc34b9553b19ae453cbab9ee64e7bceccf349
zhoudongbin <458405826@qq.com>
2024-06-11 10:01:30 +0800

    fea: cache client

---
+++ internal/pkg/cache/redis.go
@@ -0,0 +1,36 @@
+package cache
+
+import (
+	"AoniMAIOT/internal/pkg/conf"
+	"AoniMAIOT/internal/pkg/logger"
+	"github.com/go-redis/redis"
+)
+
+var gRedis *redis.Client
+
+const (
+	BlackListKey = "TokenBlackList:"
+)
+
+func InitRedis() error {
+	host := conf.Conf.GetString("redis.host")
+	pwd := conf.Conf.GetString("redis.password")
+	num := conf.Conf.GetInt("redis.num")
+
+	cli := redis.NewClient(&redis.Options{
+		Addr:     host,
+		Password: pwd,
+		DB:       num,
+	})
+	if _, err := cli.Ping().Result(); err != nil {
+		logger.Logger.Errorf("connect to redis error: %v", err)
+		return err
+	}
+
+	gRedis = cli
+	return nil
+}
+
+func GetRedis() *redis.Client {
+	return gRedis
+}
