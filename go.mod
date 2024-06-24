4548cee189c742ffdca78b9313d69bee54d466c1
zhoudongbin <458405826@qq.com>
2024-06-24 10:04:08 +0800

    fea: message queue package

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module goiot
 go 1.22.2
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.30.0
+	github.com/aws/aws-sdk-go-v2/config v1.27.21
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.21
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.33.1
 	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/gin-gonic/gin v1.10.0
@@ -21,6 +25,16 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.14 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.21.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.25.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.29.1 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
---
+++ internal/pkg/mq/sqs.go
@@ -0,0 +1,194 @@
+package mq
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"goiot/internal/pkg/conf"
+	"goiot/internal/pkg/logger"
+)
+
+type SqsConfig struct {
+	Region          string `mapstructure:"region"`
+	AccessKeyID     string `mapstructure:"accessKeyID"`
+	SecretAccessKey string `mapstructure:"secretAccessKey"`
+	UserId          string `mapstructure:"userId"`
+}
+
+type (
+	SQSPolicy struct {
+		Version   string       `json:"Version"`
+		Statement []*Statement `json:"Statement"`
+	}
+	Statement struct {
+		Effect    string     `json:"Effect"`
+		Principal any        `json:"Principal,omitempty"` // 支持json格式，也支持通配符格式
+		Action    []string   `json:"Action"`
+		Resource  []string   `json:"Resource"`
+		Condition *Condition `json:"Condition,omitempty"`
+	}
+	Principal struct {
+		AWS []string `json:"AWS,omitempty"`
+	}
+	Condition struct {
+		ArnEquals map[string]string `json:"ArnEquals,omitempty"`
+	}
+)
+
+var (
+	gSqsClient  *sqs.Client
+	gSqsConfig  SqsConfig
+	queueUrlMap map[string]string
+)
+
+func InitSqsClient() error {
+	ctx := context.Background()
+	queueUrlMap = make(map[string]string)
+	if err := conf.Conf.UnmarshalKey("aws", &gSqsConfig); err != nil {
+		logger.Logger.Errorf("unmarshal sqs config from config file error: %v", err)
+		return err
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID:     gSqsConfig.AccessKeyID,
+				SecretAccessKey: gSqsConfig.SecretAccessKey,
+			},
+		}),
+		config.WithRegion(gSqsConfig.Region),
+	)
+	if err != nil {
+		logger.Logger.Errorf("load sqs config error: %v", err)
+		return err
+	}
+
+	gSqsClient = sqs.NewFromConfig(cfg)
+
+	_, err = gSqsClient.ListQueues(ctx, &sqs.ListQueuesInput{})
+	if err != nil {
+		logger.Logger.Errorf("connect to sqs error: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func GetSqsClient() *sqs.Client {
+	return gSqsClient
+}
+
+func NormalQueueExists(ctx context.Context, queueName, s3BucketArn string) (string, error) {
+	resp, err := gSqsClient.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
+		QueueName: &queueName,
+	})
+	if err != nil {
+		var notExist *types.QueueDoesNotExist
+		if errors.As(err, &notExist) {
+			createResp, createErr := gSqsClient.CreateQueue(ctx, &sqs.CreateQueueInput{
+				QueueName: &queueName,
+			})
+			if createErr != nil {
+				return "", createErr
+			}
+
+			queueArn, err := getQueueArn(ctx, createResp.QueueUrl)
+			if err != nil {
+				return "", err
+			}
+
+			policy := SQSPolicy{
+				Version: "2012-10-17",
+				Statement: []*Statement{
+					{
+						Effect:    "Allow",
+						Action:    []string{"sqs:SendMessage"},
+						Resource:  []string{queueArn},
+						Principal: "*",
+						Condition: &Condition{
+							ArnEquals: map[string]string{
+								"aws:SourceArn": s3BucketArn,
+							},
+						},
+					},
+					{
+						Effect:    "Allow",
+						Action:    []string{"sqs:ReceiveMessage"},
+						Resource:  []string{queueArn},
+						Principal: "*",
+					},
+				},
+			}
+			policyJSON, err := json.Marshal(policy)
+			if err != nil {
+				return "", err
+			}
+
+			_, err = gSqsClient.SetQueueAttributes(ctx, &sqs.SetQueueAttributesInput{
+				QueueUrl: createResp.QueueUrl,
+				Attributes: map[string]string{
+					"Policy": string(policyJSON),
+				},
+			})
+			if err != nil {
+				return "", err
+			}
+
+			return queueArn, nil
+		}
+		return "", err
+	}
+
+	return getQueueArn(ctx, resp.QueueUrl)
+}
+
+func getQueueArn(ctx context.Context, queueUrl *string) (string, error) {
+	attrs, err := gSqsClient.GetQueueAttributes(ctx, &sqs.GetQueueAttributesInput{
+		QueueUrl:       queueUrl,
+		AttributeNames: []types.QueueAttributeName{"QueueArn"},
+	})
+	if err != nil {
+		return "", fmt.Errorf("error retrieving sqs queue attributes: %w", err)
+	}
+	return attrs.Attributes["QueueArn"], nil
+}
+
+func FIFOQueueExists(ctx context.Context, client *sqs.Client, queueName string) (string, error) {
+	resp, err := client.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
+		QueueName: &queueName,
+	})
+	if err != nil {
+		var notExist *types.QueueDoesNotExist
+		if errors.As(err, &notExist) {
+			createResp, createErr := client.CreateQueue(ctx, &sqs.CreateQueueInput{
+				QueueName: &queueName,
+				Attributes: map[string]string{
+					"FifoQueue": "true",
+				},
+				Tags: nil,
+			})
+			if createErr != nil {
+				return "", createErr
+			}
+			queueUrlMap[queueName] = *resp.QueueUrl
+			queueArn, err := getQueueArn(ctx, createResp.QueueUrl)
+			if err != nil {
+				return "", err
+			}
+			return queueArn, nil
+		}
+		return "", err
+	}
+	queueUrlMap[queueName] = *resp.QueueUrl
+	return getQueueArn(ctx, resp.QueueUrl)
+}
+
+func GetQueueUrl(queueName string) string {
+	return queueUrlMap[queueName]
+}
