9090e0028d9957b12ef4a0055c6ff899040bf68b
zhoudongbin <458405826@qq.com>
2024-07-10 10:04:20 +0800

    middleware: rate limit

--- internal/global/global.go
+++ internal/global/global.go
@@ -2,4 +2,5 @@ package global
 
 const (
 	CostCtx = "cost"
+	ErrCtx  = "error"
 )
--- internal/middleware/common.go
+++ internal/middleware/common.go
@@ -3,6 +3,8 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"goiot/internal/global"
+	"goiot/internal/pkg/errcode"
+	"golang.org/x/time/rate"
 	"time"
 )
 
@@ -12,5 +14,21 @@ func StatCost() gin.HandlerFunc {
 		c.Next()
 		cost := time.Since(start).Milliseconds()
 		c.Set(global.CostCtx, cost)
+	}
+}
+
+var limiter = rate.NewLimiter(rate.Limit(1.667), 10) // 每秒允许1.667个请求（即每分钟最多允许100个请求），瞬间最多允许10个请求，
+func RateLimit() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if hasToken, exists := c.Get("hasToken"); exists && hasToken.(bool) {
+			c.Next()
+			return
+		}
+		if !limiter.Allow() {
+			c.Set(global.ErrCtx, errcode.TooMandyRequests)
+			c.Abort()
+			return
+		}
+		c.Next()
 	}
 }
