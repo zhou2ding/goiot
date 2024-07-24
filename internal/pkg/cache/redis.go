e6612548b6ad3dd0392b4def894cbba75662dd69
zhoudongbin <458405826@qq.com>
2024-07-24 18:06:22 +0800

    fix：redis client support sentinel

--- internal/pkg/cache/redis.go
+++ internal/pkg/cache/redis.go
@@ -8,19 +8,17 @@ import (
 
 var gRedis *redis.Client
 
-const (
-	BlackListKey = "TokenBlackList:"
-)
-
 func InitRedis() error {
-	host := conf.Conf.GetString("redis.host")
+	sentinelAddrs := conf.Conf.GetStringSlice("redis.sentinel.addrs")
+	masterName := conf.Conf.GetString("redis.sentinel.master")
 	pwd := conf.Conf.GetString("redis.password")
 	num := conf.Conf.GetInt("redis.num")
 
-	cli := redis.NewClient(&redis.Options{
-		Addr:     host,
-		Password: pwd,
-		DB:       num,
+	cli := redis.NewFailoverClient(&redis.FailoverOptions{
+		MasterName:    masterName,
+		SentinelAddrs: sentinelAddrs,
+		Password:      pwd,
+		DB:            num, // 添加数据库选择
 	})
 	if _, err := cli.Ping().Result(); err != nil {
 		logger.Logger.Errorf("connect to redis error: %v", err)
