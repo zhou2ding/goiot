197d38ad140c10281d0ec4ff029b2be5e7acac20
zhoudongbin <458405826@qq.com>
2024-08-14 10:21:46 +0800

    middleware

--- apps/iotcore/api/internal/middleware/processreqrespmiddleware.go
+++ apps/iotcore/api/internal/middleware/processreqrespmiddleware.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"goiot/pkg/defs"
 	"goiot/pkg/utils"
+	"google.golang.org/grpc/metadata"
 	"net/http"
 )
 
@@ -17,7 +18,14 @@ func NewProcessReqRespMiddleware() *ProcessReqRespMiddleware {
 
 func (m *ProcessReqRespMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), defs.RequestIdCtx, utils.GetUUIDFull())
+		reqId := utils.GetUUIDFull()
+
+		ctx := metadata.AppendToOutgoingContext(r.Context(),
+			defs.RequestIdCtx, reqId,
+			defs.RemoteIpCtx, m.localIp,
+		)
+
+		ctx = context.WithValue(ctx, defs.RequestIdCtx, utils.GetUUIDFull())
 		ctx = context.WithValue(ctx, defs.RemoteIpCtx, m.localIp)
 		next(w, r.WithContext(ctx))
 	}
